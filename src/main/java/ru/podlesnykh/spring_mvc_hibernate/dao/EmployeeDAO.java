d8f28937dd439f0c835387d65773b064fb7a065e
йй <woolf_32@mail.ru>
2024-06-27 19:15:15 +0300

    remove am for two methods

--- src/main/java/ru/podlesnykh/spring_mvc_hibernate/dao/EmployeeDAO.java
+++ src/main/java/ru/podlesnykh/spring_mvc_hibernate/dao/EmployeeDAO.java
@@ -5,8 +5,8 @@ import ru.podlesnykh.spring_mvc_hibernate.entity.Employee;
 import java.util.List;
 
 public interface EmployeeDAO {
-    public List<Employee> showAllEmployee ();
-    public Employee getEmployeeById (int id);
+    List<Employee> showAllEmployee ();
+    Employee getEmployeeById (int id);
     void save(Employee employee);
     void update(int id, Employee employee);
     void delete(int id);
